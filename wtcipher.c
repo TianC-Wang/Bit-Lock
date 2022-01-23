a6e0190959e613a098c36c1f9d17d9c8f748af2d
TianC Wang <thewater2016@qq.com>
2022-01-23 17:56:00 +0800

    Tested reverse

--- wtcipher.c
+++ wtcipher.c
@@ -10,12 +10,26 @@ int8_t wtcipher_rev8(int8_t _In, int8_t _Val)
         if (WTCIPHER_8_BIT(8, _In))
             WTCIPHER_8_SET(1, ret);
     }
+    else
+    {
+        if (WTCIPHER_8_BIT(1, _In))
+            WTCIPHER_8_SET(1, ret);
+        if (WTCIPHER_8_BIT(8, _In))
+            WTCIPHER_8_SET(8, ret);
+    }
     if (WTCIPHER_8_BIT(2, _Val))
     {
         if (WTCIPHER_8_BIT(2, _In))
             WTCIPHER_8_SET(7, ret);
         if (WTCIPHER_8_BIT(7, _In))
             WTCIPHER_8_SET(2, ret);
+    }
+    else
+    {
+        if (WTCIPHER_8_BIT(2, _In))
+            WTCIPHER_8_SET(2, ret);
+        if (WTCIPHER_8_BIT(7, _In))
+            WTCIPHER_8_SET(7, ret);
     }
     if (WTCIPHER_8_BIT(3, _Val))
     {
@@ -24,12 +38,26 @@ int8_t wtcipher_rev8(int8_t _In, int8_t _Val)
         if (WTCIPHER_8_BIT(6, _In))
             WTCIPHER_8_SET(3, ret);
     }
+    else
+    {
+        if (WTCIPHER_8_BIT(3, _In))
+            WTCIPHER_8_SET(3, ret);
+        if (WTCIPHER_8_BIT(6, _In))
+            WTCIPHER_8_SET(6, ret);
+    }
     if (WTCIPHER_8_BIT(4, _Val))
     {
         if (WTCIPHER_8_BIT(4, _In))
             WTCIPHER_8_SET(5, ret);
         if (WTCIPHER_8_BIT(5, _In))
             WTCIPHER_8_SET(4, ret);
+    }
+    else
+    {
+        if (WTCIPHER_8_BIT(4, _In))
+            WTCIPHER_8_SET(4, ret);
+        if (WTCIPHER_8_BIT(5, _In))
+            WTCIPHER_8_SET(5, ret);
     }
     return ret;
 }
