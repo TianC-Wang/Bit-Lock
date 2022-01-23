27af073583d179e35372012024836d6592665bd5
TianC Wang <thewater2016@qq.com>
2022-01-23 17:34:27 +0800

    Initial commit

---
+++ wtcipher.c
@@ -0,0 +1,34 @@
+#include "wtcipher.h"
+
+int8_t wtcipher_rev8(int8_t _In, int8_t _Val)
+{
+    int8_t ret = (int8_t)0;
+    if (WTCIPHER_8_BIT(1, _Val))
+    {
+        if (WTCIPHER_8_BIT(1, _In))
+            WTCIPHER_8_SET(8, ret);
+        if (WTCIPHER_8_BIT(8, _In))
+            WTCIPHER_8_SET(1, ret);
+    }
+    if (WTCIPHER_8_BIT(2, _Val))
+    {
+        if (WTCIPHER_8_BIT(2, _In))
+            WTCIPHER_8_SET(7, ret);
+        if (WTCIPHER_8_BIT(7, _In))
+            WTCIPHER_8_SET(2, ret);
+    }
+    if (WTCIPHER_8_BIT(3, _Val))
+    {
+        if (WTCIPHER_8_BIT(3, _In))
+            WTCIPHER_8_SET(6, ret);
+        if (WTCIPHER_8_BIT(6, _In))
+            WTCIPHER_8_SET(3, ret);
+    }
+    if (WTCIPHER_8_BIT(4, _Val))
+    {
+        if (WTCIPHER_8_BIT(4, _In))
+            WTCIPHER_8_SET(5, ret);
+        if (WTCIPHER_8_BIT(5, _In))
+            WTCIPHER_8_SET(4, ret);
+    }
+}
---
+++ wtcipher.h
@@ -0,0 +1,31 @@
+#include <stdint.h>
+#include <stdbool.h>
+
+#pragma region
+#define WTCIPHER_8_LO(x) (x & (int8_t)0x0F)
+#define WTCIPHER_8_HI(x) ((x & (int8_t)0xF0) >> 4)
+#define __WTCIPHER_8_1(x) (x & (int8_t)0x01)
+#define __WTCIPHER_8_2(x) (x & (int8_t)0x02)
+#define __WTCIPHER_8_3(x) (x & (int8_t)0x04)
+#define __WTCIPHER_8_4(x) (x & (int8_t)0x08)
+#define __WTCIPHER_8_5(x) (x & (int8_t)0x10)
+#define __WTCIPHER_8_6(x) (x & (int8_t)0x20)
+#define __WTCIPHER_8_7(x) (x & (int8_t)0x40)
+#define __WTCIPHER_8_8(x) (x & (int8_t)0x80)
+#define __WTCIPHER_8_SET1(x) (x |= (int8_t)0x01)
+#define __WTCIPHER_8_SET2(x) (x |= (int8_t)0x02)
+#define __WTCIPHER_8_SET3(x) (x |= (int8_t)0x04)
+#define __WTCIPHER_8_SET4(x) (x |= (int8_t)0x08)
+#define __WTCIPHER_8_SET5(x) (x |= (int8_t)0x10)
+#define __WTCIPHER_8_SET6(x) (x |= (int8_t)0x20)
+#define __WTCIPHER_8_SET7(x) (x |= (int8_t)0x40)
+#define __WTCIPHER_8_SET8(x) (x |= (int8_t)0x80)
+#define WTCIPHER_8_BIT(b, x) __WTCIPHER_8_##b(x)
+#define WTCIPHER_8_SET(b, x) __WTCIPHER_8_SET##b(x)
+#pragma endregion
+
+/// @brief Reverse specific bits.
+/// @param _In The input.
+/// @param _Key 4 bit key.
+/// @return Reversed bits.
+int8_t wtcipher_rev8(int8_t _In, int8_t _Val);
