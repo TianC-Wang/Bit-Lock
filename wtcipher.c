ba8fc6be14b70ac9241bab323fdfe21b206c868d
TianC Wang <thewater2016@qq.com>
2022-01-23 22:03:52 +0800

    8to16, 16to8

--- wtcipher.c
+++ wtcipher.c
@@ -66,3 +66,239 @@ int8_t wtcipher_cover8(int8_t _In, int8_t _Key)
 {
     return _In ^ _Key;
 }
+
+int16_t wtcipher_8to16(int8_t* _In, int8_t _Key)
+{
+    int16_t ret = (int16_t)0;
+    if (WTCIPHER_8_BIT(1, _Key))
+    {
+        if (WTCIPHER_8_BIT(1, _In[0]))
+            WTCIPHER_16_SET(1, ret);
+        if (WTCIPHER_8_BIT(1, _In[1]))
+            WTCIPHER_16_SET(9, ret);
+    }
+    else
+    {
+        if (WTCIPHER_8_BIT(1, _In[1]))
+            WTCIPHER_16_SET(9, ret);
+        if (WTCIPHER_8_BIT(1, _In[0]))
+            WTCIPHER_16_SET(1, ret);
+    }
+    if (WTCIPHER_8_BIT(2, _Key))
+    {
+        if (WTCIPHER_8_BIT(2, _In[0]))
+            WTCIPHER_16_SET(2, ret);
+        if (WTCIPHER_8_BIT(2, _In[1]))
+            WTCIPHER_16_SET(10, ret);
+    }
+    else
+    {
+        if (WTCIPHER_8_BIT(2, _In[1]))
+            WTCIPHER_16_SET(2, ret);
+        if (WTCIPHER_8_BIT(2, _In[0]))
+            WTCIPHER_16_SET(10, ret);
+    }
+    if (WTCIPHER_8_BIT(3, _Key))
+    {
+        if (WTCIPHER_8_BIT(3, _In[0]))
+            WTCIPHER_16_SET(3, ret);
+        if (WTCIPHER_8_BIT(3, _In[1]))
+            WTCIPHER_16_SET(11, ret);
+    }
+    else
+    {
+        if (WTCIPHER_8_BIT(3, _In[1]))
+            WTCIPHER_16_SET(3, ret);
+        if (WTCIPHER_8_BIT(3, _In[0]))
+            WTCIPHER_16_SET(11, ret);
+    }
+    if (WTCIPHER_8_BIT(4, _Key))
+    {
+        if (WTCIPHER_8_BIT(4, _In[0]))
+            WTCIPHER_16_SET(4, ret);
+        if (WTCIPHER_8_BIT(4, _In[1]))
+            WTCIPHER_16_SET(12, ret);
+    }
+    else
+    {
+        if (WTCIPHER_8_BIT(4, _In[1]))
+            WTCIPHER_16_SET(4, ret);
+        if (WTCIPHER_8_BIT(4, _In[0]))
+            WTCIPHER_16_SET(12, ret);
+    }
+    if (WTCIPHER_8_BIT(5, _Key))
+    {
+        if (WTCIPHER_8_BIT(5, _In[0]))
+            WTCIPHER_16_SET(5, ret);
+        if (WTCIPHER_8_BIT(5, _In[1]))
+            WTCIPHER_16_SET(13, ret);
+    }
+    else
+    {
+        if (WTCIPHER_8_BIT(5, _In[1]))
+            WTCIPHER_16_SET(5, ret);
+        if (WTCIPHER_8_BIT(5, _In[0]))
+            WTCIPHER_16_SET(13, ret);
+    }
+    if (WTCIPHER_8_BIT(6, _Key))
+    {
+        if (WTCIPHER_8_BIT(6, _In[0]))
+            WTCIPHER_16_SET(6, ret);
+        if (WTCIPHER_8_BIT(6, _In[1]))
+            WTCIPHER_16_SET(14, ret);
+    }
+    else
+    {
+        if (WTCIPHER_8_BIT(6, _In[1]))
+            WTCIPHER_16_SET(6, ret);
+        if (WTCIPHER_8_BIT(6, _In[0]))
+            WTCIPHER_16_SET(14, ret);
+    }
+    if (WTCIPHER_8_BIT(7, _Key))
+    {
+        if (WTCIPHER_8_BIT(7, _In[0]))
+            WTCIPHER_16_SET(7, ret);
+        if (WTCIPHER_8_BIT(7, _In[1]))
+            WTCIPHER_16_SET(15, ret);
+    }
+    else
+    {
+        if (WTCIPHER_8_BIT(7, _In[1]))
+            WTCIPHER_16_SET(7, ret);
+        if (WTCIPHER_8_BIT(7, _In[0]))
+            WTCIPHER_16_SET(15, ret);
+    }
+    if (WTCIPHER_8_BIT(8, _Key))
+    {
+        if (WTCIPHER_8_BIT(8, _In[0]))
+            WTCIPHER_16_SET(8, ret);
+        if (WTCIPHER_8_BIT(8, _In[1]))
+            WTCIPHER_16_SET(16, ret);
+    }
+    else
+    {
+        if (WTCIPHER_8_BIT(8, _In[1]))
+            WTCIPHER_16_SET(8, ret);
+        if (WTCIPHER_8_BIT(8, _In[0]))
+            WTCIPHER_16_SET(16, ret);
+    }
+    return ret;
+}
+
+int16_t wtcipher_16to8(int16_t _In, int8_t _Key)
+{
+    int16_t ret = (int16_t)0;
+    if (WTCIPHER_8_BIT(1, _Key))
+    {
+        if (WTCIPHER_16_BIT(9, _In))
+            WTCIPHER_16_SET(1, ret);
+        if (WTCIPHER_16_BIT(1, _In))
+            WTCIPHER_16_SET(9, ret);
+    }
+    else
+    {
+        if (WTCIPHER_16_BIT(1, _In))
+            WTCIPHER_16_SET(1, ret);
+        if (WTCIPHER_16_BIT(9, _In))
+            WTCIPHER_16_SET(9, ret);
+    }
+    if (WTCIPHER_8_BIT(2, _Key))
+    {
+        if (WTCIPHER_16_BIT(10, _In))
+            WTCIPHER_16_SET(2, ret);
+        if (WTCIPHER_16_BIT(2, _In))
+            WTCIPHER_16_SET(10, ret);
+    }
+    else
+    {
+        if (WTCIPHER_16_BIT(2, _In))
+            WTCIPHER_16_SET(2, ret);
+        if (WTCIPHER_16_BIT(10, _In))
+            WTCIPHER_16_SET(10, ret);
+    }
+    if (WTCIPHER_8_BIT(3, _Key))
+    {
+        if (WTCIPHER_16_BIT(11, _In))
+            WTCIPHER_16_SET(3, ret);
+        if (WTCIPHER_16_BIT(3, _In))
+            WTCIPHER_16_SET(11, ret);
+    }
+    else
+    {
+        if (WTCIPHER_16_BIT(3, _In))
+            WTCIPHER_16_SET(3, ret);
+        if (WTCIPHER_16_BIT(11, _In))
+            WTCIPHER_16_SET(11, ret);
+    }
+    if (WTCIPHER_8_BIT(4, _Key))
+    {
+        if (WTCIPHER_16_BIT(12, _In))
+            WTCIPHER_16_SET(4, ret);
+        if (WTCIPHER_16_BIT(4, _In))
+            WTCIPHER_16_SET(12, ret);
+    }
+    else
+    {
+        if (WTCIPHER_16_BIT(4, _In))
+            WTCIPHER_16_SET(4, ret);
+        if (WTCIPHER_16_BIT(12, _In))
+            WTCIPHER_16_SET(12, ret);
+    }
+    if (WTCIPHER_8_BIT(5, _Key))
+    {
+        if (WTCIPHER_16_BIT(13, _In))
+            WTCIPHER_16_SET(5, ret);
+        if (WTCIPHER_16_BIT(5, _In))
+            WTCIPHER_16_SET(13, ret);
+    }
+    else
+    {
+        if (WTCIPHER_16_BIT(5, _In))
+            WTCIPHER_16_SET(5, ret);
+        if (WTCIPHER_16_BIT(13, _In))
+            WTCIPHER_16_SET(13, ret);
+    }
+    if (WTCIPHER_8_BIT(6, _Key))
+    {
+        if (WTCIPHER_16_BIT(14, _In))
+            WTCIPHER_16_SET(6, ret);
+        if (WTCIPHER_16_BIT(6, _In))
+            WTCIPHER_16_SET(14, ret);
+    }
+    else
+    {
+        if (WTCIPHER_16_BIT(6, _In))
+            WTCIPHER_16_SET(6, ret);
+        if (WTCIPHER_16_BIT(14, _In))
+            WTCIPHER_16_SET(14, ret);
+    }
+    if (WTCIPHER_8_BIT(7, _Key))
+    {
+        if (WTCIPHER_16_BIT(15, _In))
+            WTCIPHER_16_SET(7, ret);
+        if (WTCIPHER_16_BIT(7, _In))
+            WTCIPHER_16_SET(15, ret);
+    }
+    else
+    {
+        if (WTCIPHER_16_BIT(7, _In))
+            WTCIPHER_16_SET(7, ret);
+        if (WTCIPHER_16_BIT(15, _In))
+            WTCIPHER_16_SET(15, ret);
+    }
+    if (WTCIPHER_8_BIT(8, _Key))
+    {
+        if (WTCIPHER_16_BIT(16, _In))
+            WTCIPHER_16_SET(8, ret);
+        if (WTCIPHER_16_BIT(8, _In))
+            WTCIPHER_16_SET(16, ret);
+    }
+    else
+    {
+        if (WTCIPHER_16_BIT(8, _In))
+            WTCIPHER_16_SET(8, ret);
+        if (WTCIPHER_16_BIT(16, _In))
+            WTCIPHER_16_SET(16, ret);
+    }
+    return ret;
+}
--- wtcipher.h
+++ wtcipher.h
@@ -2,8 +2,6 @@
 #include <stdbool.h>
 
 #pragma region
-#define WTCIPHER_8_LO(x) (x & (int8_t)0x0F)
-#define WTCIPHER_8_HI(x) ((x & (int8_t)0xF0) >> 4)
 #define __WTCIPHER_8_OPR1(x, op) (x op (int8_t)0x01)
 #define __WTCIPHER_8_OPR2(x, op) (x op (int8_t)0x02)
 #define __WTCIPHER_8_OPR3(x, op) (x op (int8_t)0x04)
@@ -12,10 +10,45 @@
 #define __WTCIPHER_8_OPR6(x, op) (x op (int8_t)0x20)
 #define __WTCIPHER_8_OPR7(x, op) (x op (int8_t)0x40)
 #define __WTCIPHER_8_OPR8(x, op) (x op (int8_t)0x80)
+
+#define __WTCIPHER_16_OPR1(x, op) (x op (int16_t)0x0001)
+#define __WTCIPHER_16_OPR2(x, op) (x op (int16_t)0x0002)
+#define __WTCIPHER_16_OPR3(x, op) (x op (int16_t)0x0004)
+#define __WTCIPHER_16_OPR4(x, op) (x op (int16_t)0x0008)
+#define __WTCIPHER_16_OPR5(x, op) (x op (int16_t)0x0010)
+#define __WTCIPHER_16_OPR6(x, op) (x op (int16_t)0x0020)
+#define __WTCIPHER_16_OPR7(x, op) (x op (int16_t)0x0040)
+#define __WTCIPHER_16_OPR8(x, op) (x op (int16_t)0x0080)
+#define __WTCIPHER_16_OPR9(x, op) (x op (int16_t)0x0100)
+#define __WTCIPHER_16_OPR10(x, op) (x op (int16_t)0x0200)
+#define __WTCIPHER_16_OPR11(x, op) (x op (int16_t)0x0400)
+#define __WTCIPHER_16_OPR12(x, op) (x op (int16_t)0x0800)
+#define __WTCIPHER_16_OPR13(x, op) (x op (int16_t)0x1000)
+#define __WTCIPHER_16_OPR14(x, op) (x op (int16_t)0x2000)
+#define __WTCIPHER_16_OPR15(x, op) (x op (int16_t)0x4000)
+#define __WTCIPHER_16_OPR16(x, op) (x op (int16_t)0x8000)
+#pragma endregion
+#pragma region
+#define WTCIPHER_8_LO(x) (x & (int8_t)0x0F)
+#define WTCIPHER_8_HI(x) ((x & (int8_t)0xF0) >> 4)
 #define WTCIPHER_8_BIT(b, x) __WTCIPHER_8_OPR##b(x, &)
 #define WTCIPHER_8_SET(b, x) __WTCIPHER_8_OPR##b(x, |=)
 #define WTCIPHER_8_CHG(b, x) __WTCIPHER_8_OPR##b(x, ^=)
 #define WTCIPHER_8_CLR(b, x) __WTCIPHER_8_OPR##b(x, &=~)
+
+#define WTCIPHER_16_LO(x) (x & (int16_t)0x00FF)
+#define WTCIPHER_16_HI(x) ((x & (int16_t)0xFF00) >> 8)
+#define WTCIPHER_16_BIT(b, x) __WTCIPHER_16_OPR##b(x, &)
+#define WTCIPHER_16_SET(b, x) __WTCIPHER_16_OPR##b(x, |=)
+#define WTCIPHER_16_CHG(b, x) __WTCIPHER_16_OPR##b(x, ^=)
+#define WTCIPHER_16_CLR(b, x) __WTCIPHER_16_OPR##b(x, &=~)
+
+#define WTCIPHER_32_LO(x) (x & (int32_t)0x0000FFFF)
+#define WTCIPHER_32_HI(x) ((x & (int32_t)0xFFFF0000) >> 16)
+
+#define WTCIPHER_64_LO(x) (x & (int64_t)0x00000000FFFFFFFF)
+#define WTCIPHER_64_HI(x) ((x & (int64_t)0xFFFFFFFF00000000) >> 32)
+
 #pragma endregion
 
 /// @brief Reverse specific bits.
@@ -29,3 +62,15 @@ int8_t wtcipher_rev8(int8_t _In, int8_t _Key);
 /// @param _Key 8 bit key.
 /// @return Covered bits.
 int8_t wtcipher_cover8(int8_t _In, int8_t _Key);
+
+/// @brief Put 16 bits mixed together.
+/// @param _In The input.
+/// @param _Key 8 bit key.
+/// @return The combined 16 bits.
+int16_t wtcipher_8to16(int8_t* _In, int8_t _Key);
+
+/// @brief Put 16 bits reordered.
+/// @param _In The input.
+/// @param _Key 8 bit key.
+/// @return The reordered 16 bits.
+int16_t wtcipher_16to8(int16_t _In, int8_t _Key);
